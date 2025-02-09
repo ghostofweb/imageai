23102a233d4f4217ca6a229af545a3f6be7b5324
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-09 15:19:56 +0530

    Refactor Image model to include additional fields and timestamps for enhanced data structure

--- src/models/image.mode.ts
+++ src/models/image.mode.ts
@@ -1,11 +1,43 @@
-import { Schema ,Document} from "mongoose";
+import { Schema, Document, model, models } from "mongoose";
 
-interface ImageSchema extends Document{
-    title:string;
-    transformationType:string;
+interface Image extends Document {
+    title: string;
+    transformationType: string;
+    publicId: string;
+    secureUrl: string;
+    width?: number;
+    height?: number;
+    config?: object;
+    transformationUrl?: string;
+    aspectRatio?: string;
+    color?: string;
+    prompt?: string;
+    author?: {
+        _id:string;
+        firstName:string;
+        lastname:string;
+    }
+    createdAt: Date;
+    updatedAt: Date;
 }
-const ImageSchema = new Schema<ImageSchema>({
-    title:{type:String,required:true},
-    transformationType:{type:String,required:true},
 
-})
+const ImageSchema = new Schema<Image>(
+    {
+        title: { type: String, required: true },
+        transformationType: { type: String, required: true },
+        publicId: { type: String, required: true },
+        secureUrl: { type: String, required: true }, 
+        width: { type: Number },
+        height: { type: Number },
+        config: { type: Object, default: {} },
+        transformationUrl: { type: String }, 
+        aspectRatio: { type: String },
+        color: { type: String },
+        prompt: { type: String },
+        author: { type: Schema.Types.ObjectId, ref: "User" },
+    },
+    { timestamps: true } 
+);
+const Image = models?.Image || model<Image>("Image",ImageSchema)
+
+export default Image;
