7a96d83661109b86e904a414594281810448a0a0
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-14 15:44:07 +0530

    Add authentication middleware for public routes in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+
 export default clerkMiddleware();
+
+export const authMiddleware = {
+  publicRoutes: ["/api/webhooks/clerk"],
+};
 
 export const config = {
   matcher: [
