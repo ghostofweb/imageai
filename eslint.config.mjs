53f21849a7b885e310c288b84e88a1300aad5868
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-14 14:44:42 +0530

    Update ESLint configuration to disable specific rules for improved flexibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-spread": "off",
+      "no-prototype-builtins": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
