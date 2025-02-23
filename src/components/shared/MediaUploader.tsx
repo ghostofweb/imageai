fcfb376e1e57b90d83608d63d215ba22e2205026
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-23 23:18:03 +0530

    Add MediaUploader component and integrate with TransformationForm; update package.json for new dependencies

---
+++ src/components/shared/MediaUploader.tsx
@@ -0,0 +1,70 @@
+import { useToast } from '@/hooks/use-toast'
+import React from 'react'
+import { Button } from '../ui/Button2'
+import { CldUploadWidget } from 'next-cloudinary'
+import Image from 'next/image'
+
+type MediaUploaderProps = {
+    onValueChange: (value: string) => void;
+    setImage: React.Dispatch<any>;
+    image: any;
+    publicId: string;
+    type: string;
+}
+const MediaUploader = ({onValueChange,setImage,image,publicId,type}:MediaUploaderProps) => {
+    const {toast} = useToast()
+    
+    const onUploadSuccessHandler = (result:any) => {
+        toast({
+            title: "Image Uploaded Successfully",
+            description: "1 credit was deducted from your account",
+            duration: 5000,
+            className:'error-toast'
+        })
+    }
+    const onUploadErrorHandler = () => {
+        toast({
+            title: "Something went wrong while uploading the image",
+            description: "Please try again",
+            duration: 5000,
+            className:'error-toast'
+        })
+    }
+  return (
+    <div>
+       <CldUploadWidget
+       uploadPreset='imageai'
+       options={{
+        multiple:false,
+        resourceType:"image",
+       }}
+       onSuccess={onUploadSuccessHandler}
+       onError={onUploadErrorHandler}
+       >
+        {({open})=>(
+            <div className='flex flex-col gap-4'>
+                <h3 className='h3-bold text-dark-600'>
+                    Original 
+                </h3>
+                {publicId ?(
+                <>
+                HERE IS THE IMAGE
+                </>
+            ):(
+            <div className='media-uploader_cta' onClick={()=>{
+                open()
+            }}>
+           <div className='media-uploader_cta-image'>
+                <Image src="/assets/icons/add.svg" alt='Add Image' width={24} height={24}/>
+           </div>
+                <p className='p-14-medium'>Click here to Upload the Image</p>
+            </div>
+        )}
+            </div>
+        )}
+       </CldUploadWidget>
+    </div>
+  )
+}
+
+export default MediaUploader
--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -25,6 +25,7 @@ import {
 } from "@/components/ui/select"
 import { AspectRatioKey, debounce, deepMergeObjects } from '@/lib/utils'
 import { updateCredits } from '@/lib/actions/user.actions'
+import MediaUploader from './MediaUploader'
 
 type Transformations = {
   title: string;
@@ -197,6 +198,23 @@ const TransformationForm = ({
           )}
             </div>
           )}
+
+          <div className='media-uploader-field'>
+          <CustomField
+          control={form.control}
+          name='publicId'
+          className='flex size-full flex-col'
+          render={({field})=>(
+            <MediaUploader
+            onValueChange = {field.onChange}
+            setImage={setImage}
+            publicId={field.value}
+            image={image}
+            type={type}
+            />
+          )}
+          />
+          </div>
           <div className='flex flex-col gap-4'>
           <Button type='submit' className='submit-button'
           disabled={isTranforming || newTransformation === null} onClick={onTranformHandler}>
