88f64bd6f5da48517e61b8c84d585510889d5751
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-27 17:32:20 +0530

    Refactor Page component to improve type definitions for params and searchParams, and remove unnecessary await for params

--- src/app/(root)/transformations/[id]/update/page.tsx
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -7,8 +7,17 @@ import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
 import { getImageById } from "@/lib/actions/image.actions";
 
-const Page = async ({ params }: SearchParamProps) => {
-  const { id } = await params;
+type PageProps = {
+  params: {
+    id: string;
+    type: TransformationTypeKey;
+  };
+  searchParams?: Record<string, string>;
+};
+
+const Page = async ({ params }: PageProps) => {
+  // No need to await params here
+  const { id } = params;
   const { userId } = await auth();
 
   if (!userId) redirect("/sign-in");
